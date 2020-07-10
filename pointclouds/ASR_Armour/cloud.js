2cbd2830a2b39faca716abb8be1b3ea60a78ec88
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2020-07-10 12:14:09 +1000

    New point cloud

---
+++ pointclouds/ASR_Armour/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm",
+    "points": 1335094,
+    "boundingBox": {
+        "lx": 583950.7,
+        "ly": 6124497.1,
+        "lz": -10.0,
+        "ux": 584112.1,
+        "uy": 6124658.5,
+        "uz": 151.40000000002329
+    },
+    "tightBoundingBox": {
+        "lx": 583950.7,
+        "ly": 6124497.1,
+        "lz": -10.0,
+        "ux": 584112.1,
+        "uy": 6124627.8,
+        "uz": -1.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.397765040397644,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
