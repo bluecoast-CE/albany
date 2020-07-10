f2b85e1285aa74ffd627d7fe6da2575128848d6d
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2020-07-10 12:33:12 +1000

    Final ASR upload

---
+++ pointclouds/ASR_Sandbelly/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm",
+    "points": 677339,
+    "boundingBox": {
+        "lx": 583961.0,
+        "ly": 6124538.0,
+        "lz": -7.5,
+        "ux": 584103.9,
+        "uy": 6124680.9,
+        "uz": 135.40000000002329
+    },
+    "tightBoundingBox": {
+        "lx": 583961.0,
+        "ly": 6124538.0,
+        "lz": -7.5,
+        "ux": 584103.9,
+        "uy": 6124616.2,
+        "uz": -4.5
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.2375502586364747,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
