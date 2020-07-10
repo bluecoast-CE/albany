f9f7b729a3b81dea267501f19cb28e2126df84c0
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2020-07-10 12:28:26 +1000

    Upload core

---
+++ pointclouds/ASR_Core/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm",
+    "points": 1139948,
+    "boundingBox": {
+        "lx": 583956.6,
+        "ly": 6124502.9,
+        "lz": -7.559,
+        "ux": 584109.3,
+        "uy": 6124655.600000001,
+        "uz": 145.14100000006986
+    },
+    "tightBoundingBox": {
+        "lx": 583956.6,
+        "ly": 6124502.9,
+        "lz": -7.559,
+        "ux": 584109.3,
+        "uy": 6124620.6,
+        "uz": -3.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.3224208354949952,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
